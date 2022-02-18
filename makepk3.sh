320efa9a1365afa3bc5ed3b45305cb031b2a92c6
SeiZurE <s-e-i-z-u-r-e@yandex.com>
2022-02-18 19:17:12 +0200

    makepk3.sh: removed tag dependence on pk3 filename

--- makepk3.sh
+++ makepk3.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 RATDIR=~/.openarena/rat
 BRANCH=$(git branch --show-current)
-TAG=$(git tag -l --points-at HEAD)
-ZIPFILENAME=zz-soundswannabees-$BRANCH-$TAG.pk3
+ZIPFILENAME=zz-soundswannabees-$BRANCH.pk3
 
 zip -r  $RATDIR/$ZIPFILENAME sound -x sound/.gitignore
