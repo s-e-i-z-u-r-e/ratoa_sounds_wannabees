dbfb0f9c7b59b7f21910d6132c09c3f5cfb5f054
SeiZurE <s-e-i-z-u-r-e@yandex.com>
2022-02-22 21:15:29 +0200

    lg_fire2.wav: quieter explosion (same "whoosh" levels)
    makepk3.sh: removed the tag part from pk3 filenaming

--- makepk3.sh
+++ makepk3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 RATDIR=~/.openarena/rat
 BRANCH=$(git branch --show-current)
-TAG=$(git tag -l --points-at HEAD)
-ZIPFILENAME=zzz-soundswannabees-$BRANCH-$TAG.pk3
+#TAG=$(git tag -l --points-at HEAD)
+ZIPFILENAME=zzz-soundswannabees-$BRANCH.pk3
 
 zip -r  $RATDIR/$ZIPFILENAME sound -x sound/.gitignore
